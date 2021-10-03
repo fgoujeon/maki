70cea40b3ff638a2413e19adfd4ad1fbc09a1261
Florian Goujeon <florian.goujeon@gmail.com>
2021-10-03 12:00:37 +0200

    Add FGFSM_DECLARE_FSM_CLASS and FGFSM_DEFINE_FSM_CLASS macro functions

---
+++ fgfsm/include/fgfsm/fsm_class.hpp
@@ -0,0 +1,55 @@
+//Copyright Florian Goujeon 2021.
+//Distributed under the Boost Software License, Version 1.0.
+//(See accompanying file LICENSE or copy at
+//https://www.boost.org/LICENSE_1_0.txt)
+//Official repository: https://github.com/fgoujeon/fgfsm
+
+#ifndef FGFSM_FSM_CLASS_HPP
+#define FGFSM_FSM_CLASS_HPP
+
+//Expand to an fgfsm internal name to avoid name conflicts with user stuff
+#define _FGFSM(NAME) _fgfsm_private_##NAME
+
+//Forward declare FSM class and related functions to break circular dependency
+#define FGFSM_DECLARE_FSM_CLASS(CLASS_NAME) \
+    class CLASS_NAME; \
+    \
+    template<class _FGFSM(Event)> \
+    void process_event \
+    ( \
+        CLASS_NAME* _FGFSM(pfsm), \
+        const _FGFSM(Event)& _FGFSM(event) \
+    );
+
+//Define FSM class and related functions to break circular dependency
+#define FGFSM_DEFINE_FSM_CLASS(CLASS_NAME, IMPL_TYPE_NAME) \
+    class CLASS_NAME \
+    { \
+        public: \
+            template<class... _FGFSM(Args)> \
+            CLASS_NAME(_FGFSM(Args)&&... _FGFSM(args)): \
+                _FGFSM(impl_)(_FGFSM(args)...) \
+            { \
+            } \
+ \
+            template<class _FGFSM(Event)> \
+            void process_event(const _FGFSM(Event)& _FGFSM(event)) \
+            { \
+                _FGFSM(impl_).process_event(_FGFSM(event)); \
+            } \
+ \
+        private: \
+            IMPL_TYPE_NAME _FGFSM(impl_); \
+    }; \
+ \
+    template<class _FGFSM(Event)> \
+    void process_event \
+    ( \
+        CLASS_NAME* const _FGFSM(pfsm), \
+        const _FGFSM(Event)& _FGFSM(event) \
+    ) \
+    { \
+        _FGFSM(pfsm)->process_event(_FGFSM(event)); \
+    }
+
+#endif
--- test/src/test_cases/recursive_process_event.cpp
+++ test/src/test_cases/recursive_process_event.cpp
@@ -9,34 +9,18 @@
 
 namespace
 {
-    class robot;
-
-    template<class Event>
-    void process_event(robot& r, const Event& event);
+    FGFSM_DECLARE_FSM_CLASS(fsm)
 
     struct context
     {
-        context(robot& rob):
-            rob(rob)
-        {
-        }
-
-        template<class Event>
-        void process_event(const Event& event)
-        {
-            ::process_event(rob, event);
-        }
-
-        robot& rob;
+        fsm* pfsm = nullptr;
         std::string output;
     };
 
     namespace events
     {
         struct start_button_press{};
-
         struct quick_start_button_press{};
-
         struct end_of_loading{};
     }
 
@@ -90,59 +74,30 @@ namespace
             template<class StartState, class Event, class TargetState>
             void operator()(StartState&, Event&, TargetState&)
             {
-                ctx.process_event(events::end_of_loading{});
+                process_event(ctx.pfsm, events::end_of_loading{});
             }
 
             context& ctx;
         };
     }
 
-    class robot
-    {
-        private:
-            using transition_table = fgfsm::transition_table
-            <
-                fgfsm::row<states::idle,    events::start_button_press,       states::loading>,
-                fgfsm::row<states::idle,    events::quick_start_button_press, states::loading, actions::skip_loading>,
-                fgfsm::row<states::loading, events::end_of_loading,           states::ready>
-            >;
+    using transition_table = fgfsm::transition_table
+    <
+        fgfsm::row<states::idle,    events::start_button_press,       states::loading>,
+        fgfsm::row<states::idle,    events::quick_start_button_press, states::loading,  actions::skip_loading>,
+        fgfsm::row<states::loading, events::end_of_loading,           states::ready>
+    >;
 
-            using fsm = fgfsm::fsm<transition_table>;
-
-        public:
-            robot():
-                ctx_{*this},
-                fsm_{ctx_}
-            {
-            }
-
-            template<class Event>
-            void process_event(const Event& event)
-            {
-                fsm_.process_event(event);
-            }
-
-            const std::string get_output() const
-            {
-                return ctx_.output;
-            }
-
-        private:
-            context ctx_;
-            fsm fsm_;
-    };
-
-    template<class Event>
-    void process_event(robot& r, const Event& event)
-    {
-        r.process_event(event);
-    }
+    using fsm_impl = fgfsm::fsm<transition_table>;
+    FGFSM_DEFINE_FSM_CLASS(fsm, fsm_impl)
 }
 
 TEST_CASE("recursive process_event")
 {
-    auto rob = robot{};
+    auto ctx = context{};
+    auto sm = fsm{ctx};
+    ctx.pfsm = &sm;
 
-    rob.process_event(events::quick_start_button_press{});
-    REQUIRE(rob.get_output() == "idle::on_exit;loading::on_entry;loading::on_exit;ready::on_entry;");
+    sm.process_event(events::quick_start_button_press{});
+    REQUIRE(ctx.output == "idle::on_exit;loading::on_entry;loading::on_exit;ready::on_entry;");
 }

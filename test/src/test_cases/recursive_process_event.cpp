0b01ddd747b98319eb2fddd2526327c81b8eb032
Florian Goujeon <florian.goujeon@gmail.com>
2022-02-06 10:36:44 +0100

    Update copyright notice

--- test/src/test_cases/recursive_process_event.cpp
+++ test/src/test_cases/recursive_process_event.cpp
@@ -1,4 +1,4 @@
-//Copyright Florian Goujeon 2021.
+//Copyright Florian Goujeon 2021 - 2022.
 //Distributed under the Boost Software License, Version 1.0.
 //(See accompanying file LICENSE or copy at
 //https://www.boost.org/LICENSE_1_0.txt)

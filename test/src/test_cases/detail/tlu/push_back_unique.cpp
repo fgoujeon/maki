ec16175339cedf2d434b976ddf830bbe9288760f
Florian Goujeon <florian.goujeon@gmail.com>
2023-02-07 18:26:19 +0100

    Update copyright notice

--- test/src/test_cases/detail/tlu/push_back_unique.cpp
+++ test/src/test_cases/detail/tlu/push_back_unique.cpp
@@ -1,4 +1,4 @@
-//Copyright Florian Goujeon 2021 - 2022.
+//Copyright Florian Goujeon 2021 - 2023.
 //Distributed under the Boost Software License, Version 1.0.
 //(See accompanying file LICENSE or copy at
 //https://www.boost.org/LICENSE_1_0.txt)

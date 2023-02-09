184306c1ca327f7441e791365b42eacb4f78ce06
Florian Goujeon <florian.goujeon@gmail.com>
2023-02-09 19:35:23 +0100

    Simplify transition_table_fn_list_t

--- awesm/include/awesm/detail/sm_traits.hpp
+++ awesm/include/awesm/detail/sm_traits.hpp
@@ -7,15 +7,42 @@
 #ifndef AWESM_DETAIL_SM_TRAITS_HPP
 #define AWESM_DETAIL_SM_TRAITS_HPP
 
-#include "../transition_table.hpp"
 #include "clu.hpp"
+#include "../transition_table.hpp"
 #include <type_traits>
 
 namespace awesm::detail::sm_traits
 {
 
+namespace transition_table_fn_list_detail
+{
+    template<class R>
+    using nullary_fn_ptr_t = R(*)();
+
+    /*
+    T is either:
+    - a pointer to a function returning an instance of transition_table_t;
+    - an instance of transition_table_list_t.
+    */
+    template<class T, const auto& V>
+    struct helper
+    {
+        using type = T;
+    };
+
+    template<class... Transitions, const auto& TransitionTableFn>
+    struct helper<nullary_fn_ptr_t<transition_table_t<Transitions...>>, TransitionTableFn>
+    {
+        using type = transition_table_list_t<TransitionTableFn>;
+    };
+}
+
 template<class Sm>
-using transition_table_fn_list_t = to_transition_table_fn_list_t<Sm::conf::transition_table_fn>;
+using transition_table_fn_list_t = typename transition_table_fn_list_detail::helper
+<
+    std::decay_t<decltype(Sm::conf::transition_table_fn)>,
+    Sm::conf::transition_table_fn
+>::type;
 
 template<class Sm>
 inline constexpr auto region_count_v = clu::size_v<transition_table_fn_list_t<Sm>>;
--- awesm/include/awesm/transition_table.hpp
+++ awesm/include/awesm/transition_table.hpp
@@ -74,32 +74,6 @@ struct transition_table_list_t{};
 template<auto... Fns>
 inline constexpr auto transition_table_list = transition_table_list_t<Fns...>{};
 
-namespace detail
-{
-    template<class T, const auto& V>
-    struct to_transition_table_fn_list_helper;
-
-    template<class... Transitions, const auto& TransitionTableFn>
-    struct to_transition_table_fn_list_helper<transition_table_t<Transitions...>(*)(), TransitionTableFn>
-    {
-        using type = transition_table_list_t<TransitionTableFn>;
-    };
-
-    template<auto... Fns, const auto& V>
-    struct to_transition_table_fn_list_helper<transition_table_list_t<Fns...>, V>
-    {
-        using type = transition_table_list_t<Fns...>;
-    };
-
-    /*
-    The type of V is either:
-    - a pointer to a function returning an instance of transition_table_t;
-    - an instance of transition_table_list_t.
-    */
-    template<const auto& V>
-    using to_transition_table_fn_list_t = typename to_transition_table_fn_list_helper<std::decay_t<decltype(V)>, V>::type;
-}
-
 } //namespace
 
 #endif

c0ea49133764fff7af035f7881e98403b29fc9ad
Florian Goujeon <florian.goujeon@gmail.com>
2023-03-05 23:08:18 +0100

    Lazily evaluate has_on_event::value for shorter build time

--- awesm/include/awesm/detail/state_traits.hpp
+++ awesm/include/awesm/detail/state_traits.hpp
@@ -9,6 +9,7 @@
 
 #include "subsm_fwd.hpp"
 #include "overload_priority.hpp"
+#include "alternative.hpp"
 #include "tlu.hpp"
 #include "../type_filters.hpp"
 #include "../state_conf.hpp"
@@ -54,44 +55,50 @@ constexpr auto requires_on_exit_v =
     tlu::get_f_t<typename State::conf, sm_option::on_exit>::value
 ;
 
+
 //on_event
 
 static_assert(static_cast<int>(sm_option::on_event) == static_cast<int>(state_option::on_event));
 static_assert(static_cast<int>(sm_option::on_event_auto) == static_cast<int>(state_option::on_event_auto));
 
-template<class State, class Event>
 class has_on_event
 {
 private:
-    template<class State2 = State, class Event2 = Event>
+    template<class State, class Event>
     static constexpr auto has_impl(overload_priority::high /*unused*/) ->
-        decltype(std::declval<State2&>().on_event(std::declval<const Event2&>()), bool())
+        decltype(std::declval<State&>().on_event(std::declval<const Event&>()), bool())
     {
         return true;
     }
 
+    template<class State, class Event>
     static constexpr bool has_impl(overload_priority::low /*unused*/)
     {
         return false;
     }
 
 public:
-    static constexpr auto value = has_impl(overload_priority::probe);
+    template<class State, class Event> //Template here for lazy evaluation
+    static constexpr auto value = has_impl<State, Event>(overload_priority::probe);
 };
 
-template<class State, class Event>
-inline constexpr auto has_on_event_v = has_on_event<State, Event>::value;
-
-template<class State, class Event>
-constexpr auto requires_on_event_v =
-    tlu::get_f_t<typename State::conf, sm_option::on_event_auto>::value ?
-    has_on_event_v<State, Event> :
-    matches_any_filter_v
+struct matches_on_event_filter
+{
+    template<class State, class Event> //Template here for lazy evaluation
+    static constexpr auto value = matches_any_filter_v
     <
         Event,
         tlu::get_f_t<typename State::conf, sm_option::on_event>
-    >
-;
+    >;
+};
+
+template<class State, class Event>
+constexpr auto requires_on_event_v = alternative_t
+<
+    tlu::get_f_t<typename State::conf, sm_option::on_event_auto>::value,
+    has_on_event,
+    matches_on_event_filter
+>::template value<State, Event>;
 
 } //namespace
 

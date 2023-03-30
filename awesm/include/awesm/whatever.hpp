ec9da18cb9084b2a6bfe0253f2af9d2fb405970b
Florian Goujeon <florian.goujeon@gmail.com>
2023-03-30 18:27:30 +0200

    Document whatever

--- awesm/include/awesm/whatever.hpp
+++ awesm/include/awesm/whatever.hpp
@@ -10,8 +10,42 @@
 namespace awesm
 {
 
+/**
+@defgroup Utility
+Some general-purpose utility types and functions that can be handy when using
+the AweSM API.
+*/
+
+/**
+@ingroup Utility
+An empty struct with a generic unary constructor that does nothing.
+It is typically used in function parameter lists for parameters whose type and
+value are unused.
+
+For example, here is an action that only needs the \ref sm and the context, but
+doesn't care about the event:
+```cpp
+template<class Sm>
+void some_action(Sm& sm, context& ctx, awesm::whatever event)
+{
+    //...
+}
+```
+
+This is a slightly more concise way of writing this:
+```cpp
+template<class Sm, class Event>
+void some_action(Sm& sm, context& ctx, const Event& event)
+{
+    //...
+}
+```
+*/
 struct whatever
 {
+    /**
+    @brief Does nothing.
+    */
     template<class T>
     constexpr whatever(const T& /*unused*/)
     {

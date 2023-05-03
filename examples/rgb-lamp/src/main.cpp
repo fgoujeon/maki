c671aa2a5d20c8949926d9e7cb57496549271f94
Florian Goujeon <florian.goujeon@gmail.com>
2023-05-03 06:20:19 +0200

    Revert "Fix MSVC 2017 segfault"
    This reverts commit e94042e30446ce817298c757e3278141876b9260.

--- examples/rgb-lamp/src/main.cpp
+++ examples/rgb-lamp/src/main.cpp
@@ -169,25 +169,16 @@ namespace actions
         ctx.led.set_color(rgb_led::color::off);
     }
 
-    void turn_light_white(context& ctx)
+    //We can of course factorize with a template.
+    template<auto Color>
+    void turn_light_tpl(context& ctx)
     {
-        ctx.led.set_color(rgb_led::color::white);
+        ctx.led.set_color(Color);
     }
-
-    void turn_light_red(context& ctx)
-    {
-        ctx.led.set_color(rgb_led::color::red);
-    }
-
-    void turn_light_green(context& ctx)
-    {
-        ctx.led.set_color(rgb_led::color::green);
-    }
-
-    void turn_light_blue(context& ctx)
-    {
-        ctx.led.set_color(rgb_led::color::blue);
-    }
+    constexpr auto turn_light_white = turn_light_tpl<rgb_led::color::white>;
+    constexpr auto turn_light_red   = turn_light_tpl<rgb_led::color::red>;
+    constexpr auto turn_light_green = turn_light_tpl<rgb_led::color::green>;
+    constexpr auto turn_light_blue  = turn_light_tpl<rgb_led::color::blue>;
 }
 
 /*

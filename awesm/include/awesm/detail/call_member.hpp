766ca0a347f373680b863a14d7ef07241f10199d
Florian Goujeon <florian.goujeon@gmail.com>
2023-05-31 19:26:37 +0200

    Improve call_member.hpp

--- awesm/include/awesm/detail/call_member.hpp
+++ awesm/include/awesm/detail/call_member.hpp
@@ -10,11 +10,37 @@
 #include "type_traits.hpp"
 #include "state_traits.hpp"
 #include "subsm_fwd.hpp"
+#include "overload_priority.hpp"
 #include <type_traits>
 #include <utility>
 
 namespace awesm::detail
 {
+
+#define AWESM_DETAIL_GENERATE_HAS_MEMBER_FUNCTION(name) /*NOLINT*/ \
+    template<class State, class... Args> \
+    constexpr auto has_##name##_impl(overload_priority::high /*unused*/) -> \
+        decltype(std::declval<State>().name(std::declval<Args>()...), bool()) \
+    { \
+        return true; \
+    } \
+ \
+    template<class State, class... Args> \
+    constexpr bool has_##name##_impl(overload_priority::low /*unused*/) \
+    { \
+        return false; \
+    } \
+ \
+    template<class State, class... Args> \
+    constexpr bool has_##name() \
+    { \
+        return has_##name##_impl<State, Args...>(overload_priority::probe); \
+    }
+
+AWESM_DETAIL_GENERATE_HAS_MEMBER_FUNCTION(on_entry)
+AWESM_DETAIL_GENERATE_HAS_MEMBER_FUNCTION(on_exit)
+
+#undef AWESM_DETAIL_GENERATE_HAS_MEMBER_FUNCTION
 
 template<class State, class Sm, class Event>
 auto call_on_entry_impl(State& state, Sm* pmach, const Event* pevent) ->
@@ -47,7 +73,23 @@ void call_on_entry
 {
     if constexpr(state_traits::requires_on_entry<State>())
     {
-        call_on_entry_impl(state, &mach, &event);
+        if constexpr(has_on_entry<State&, Sm&, const Event&>())
+        {
+            state.on_entry(mach, event);
+        }
+        else if constexpr(has_on_entry<State&, const Event&>())
+        {
+            state.on_entry(event);
+        }
+        else if constexpr(has_on_entry<State&>())
+        {
+            state.on_entry();
+        }
+        else
+        {
+            constexpr auto is_false = sizeof(State) == 0;
+            static_assert(is_false, "No valid on_entry() signature found in state");
+        }
     }
 }
 
@@ -79,27 +121,6 @@ bool call_on_event(State& state, const Event& event)
 }
 
 template<class State, class Sm, class Event>
-auto call_on_exit_impl(State& state, Sm* pmach, const Event* pevent) ->
-    decltype(state.on_exit(*pmach, *pevent))
-{
-    state.on_exit(*pmach, *pevent);
-}
-
-template<class State, class Event>
-auto call_on_exit_impl(State& state, void* /*pmach*/, const Event* pevent) ->
-    decltype(state.on_exit(*pevent))
-{
-    state.on_exit(*pevent);
-}
-
-template<class State>
-auto call_on_exit_impl(State& state, void* /*pmach*/, const void* /*pevent*/) ->
-    decltype(state.on_exit())
-{
-    state.on_exit();
-}
-
-template<class State, class Sm, class Event>
 void call_on_exit
 (
     [[maybe_unused]] State& state,
@@ -109,7 +130,23 @@ void call_on_exit
 {
     if constexpr(state_traits::requires_on_exit<State>())
     {
-        call_on_exit_impl(state, &mach, &event);
+        if constexpr(has_on_exit<State&, Sm&, const Event&>())
+        {
+            state.on_exit(mach, event);
+        }
+        else if constexpr(has_on_exit<State&, const Event&>())
+        {
+            state.on_exit(event);
+        }
+        else if constexpr(has_on_exit<State&>())
+        {
+            state.on_exit();
+        }
+        else
+        {
+            constexpr auto is_false = sizeof(State) == 0;
+            static_assert(is_false, "No valid on_exit() signature found in state");
+        }
     }
 }
 
@@ -138,6 +175,11 @@ auto call_action_or_guard
     else if constexpr(is_nullary_v<fn_t>)
     {
         return Fn();
+    }
+    else
+    {
+        constexpr auto is_false = sizeof(Context) == 0;
+        static_assert(is_false, "No valid signature found for action/guard");
     }
 }
 

325468671f0d9b27196dbad7b14cfc37544ec1d8
Florian Goujeon <florian.goujeon@gmail.com>
2023-10-01 17:41:28 +0200

    Sort machine_conf member variables in alphabetical order

--- maki/include/maki/machine_conf.hpp
+++ maki/include/maki/machine_conf.hpp
@@ -115,66 +115,38 @@ struct machine_conf
     ContextType context_type; //NOLINT(misc-non-private-member-variables-in-classes)
 
     /**
-    @brief Disables run-to-completion.
+    @brief Requires the @ref machine to call a user-provided `on_entry()` member
+    function whenever it starts.
 
-    This makes the state machine much faster, but you have to make sure you
-    <b>never</b> call @ref machine::process_event() recursively.
-
-    Use it at your own risk!
-    */
-    bool run_to_completion = true; //NOLINT(misc-non-private-member-variables-in-classes)
-
-    /**
-    @brief Requires the @ref machine to call a user-provided `pretty_name()` static
-    member function to get the pretty name of the state machine.
-
-    See `maki::pretty_name`.
+    One of these expressions must be valid, for every given event type:
+    @code
+    machine_def.on_entry(fsm, event);
+    machine_def.on_entry(event);
+    machine_def.on_entry();
+    @endcode
 
     Example:
     @code
     struct machine_def
     {
         static constexpr auto conf = machine_conf_c
-            .enable_pretty_name()
+            .enable_on_entry()
             //...
         ;
 
-        static auto pretty_name()
+        void on_entry(const event_type_0& event)
         {
-            return "Main FSM";
+            //...
         }
 
-        //...
-    };
-    @endcode
-    */
-    bool pretty_name_fn = false; //NOLINT(misc-non-private-member-variables-in-classes)
-
-    /**
-    @brief Requires the @ref machine to call a user-provided `on_exception()`
-    member function whenever it catches an exception.
-
-    The following expression must be valid:
-    @code
-    machine_def.on_exception(std::current_exception());
-    @endcode
-
-    If this option isn't set, the @ref machine will send itself a @ref
-    events::exception event, like so:
-    @code
-    process_event(events::exception{std::current_exception()});
-    @endcode
-
-    Example:
-    @code
-    struct machine_def
-    {
-        static constexpr auto conf = machine_conf_c
-            .enable_pretty_name()
+        template<class Sm>
+        void on_entry(Sm& fsm, const event_type_1& event)
+        {
             //...
-        ;
+        }
 
-        void on_exception(const std::exception_ptr& eptr)
+        //For all other event types
+        void on_entry()
         {
             //...
         }
@@ -183,66 +155,7 @@ struct machine_conf
     };
     @endcode
     */
-    bool on_exception = false; //NOLINT(misc-non-private-member-variables-in-classes)
-
-    /**
-    @brief Requires the @ref machine to call a user-provided `on_unprocessed()`
-    member function whenever a call to @ref machine::process_event() doesn't lead to
-    any state transition.
-
-    The said member function must have the following form:
-    @code
-    void on_unprocessed(const event_type& event);
-    @endcode
-
-    State machine definitions typically define several overloads of this
-    function, for all events of interest.
-
-    Example:
-    @code
-    struct machine_def
-    {
-        static constexpr auto conf = machine_conf_c
-            .enable_on_unprocessed()
-            //...
-        ;
-
-        void on_unprocessed(const some_event_type& event)
-        {
-            //...
-        }
-
-        void on_unprocessed(const some_other_event_type& event)
-        {
-            //...
-        }
-
-        //Ignore all other event types
-        template<class Event>
-        void on_unprocessed(const Event&)
-        {
-            //nothing
-        }
-
-        //...
-    };
-    @endcode
-
-    */
-    bool on_unprocessed = false; //NOLINT(misc-non-private-member-variables-in-classes)
-
-    /**
-    @brief Maximum object alignment requirement for the run-to-completion event
-    queue to enable small object optimization (and thus avoid an extra memory
-    allocation).
-    */
-    std::size_t small_event_max_align = 8; //NOLINT(misc-non-private-member-variables-in-classes, cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-
-    /**
-    @brief Maximum object size for the run-to-completion event queue to enable
-    small object optimization (and thus avoid an extra memory allocation).
-    */
-    std::size_t small_event_max_size = 16; //NOLINT(misc-non-private-member-variables-in-classes, cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    bool on_entry = false; //NOLINT(misc-non-private-member-variables-in-classes)
 
     /**
     @brief Requires the @ref machine to call a user-provided `on_event()` member
@@ -324,14 +237,18 @@ struct machine_conf
     bool on_event_auto = false; //NOLINT(misc-non-private-member-variables-in-classes)
 
     /**
-    @brief Requires the @ref machine to call a user-provided `on_entry()` member
-    function whenever it starts.
+    @brief Requires the @ref machine to call a user-provided `on_exception()`
+    member function whenever it catches an exception.
 
-    One of these expressions must be valid, for every given event type:
+    The following expression must be valid:
     @code
-    machine_def.on_entry(fsm, event);
-    machine_def.on_entry(event);
-    machine_def.on_entry();
+    machine_def.on_exception(std::current_exception());
+    @endcode
+
+    If this option isn't set, the @ref machine will send itself a @ref
+    events::exception event, like so:
+    @code
+    process_event(events::exception{std::current_exception()});
     @endcode
 
     Example:
@@ -339,23 +256,11 @@ struct machine_conf
     struct machine_def
     {
         static constexpr auto conf = machine_conf_c
-            .enable_on_entry()
+            .enable_pretty_name()
             //...
         ;
 
-        void on_entry(const event_type_0& event)
-        {
-            //...
-        }
-
-        template<class Sm>
-        void on_entry(Sm& fsm, const event_type_1& event)
-        {
-            //...
-        }
-
-        //For all other event types
-        void on_entry()
+        void on_exception(const std::exception_ptr& eptr)
         {
             //...
         }
@@ -364,7 +269,7 @@ struct machine_conf
     };
     @endcode
     */
-    bool on_entry = false; //NOLINT(misc-non-private-member-variables-in-classes)
+    bool on_exception = false; //NOLINT(misc-non-private-member-variables-in-classes)
 
     /**
     @brief Requires the @ref machine to call a user-provided `on_exit()` member
@@ -410,6 +315,100 @@ struct machine_conf
     bool on_exit = false; //NOLINT(misc-non-private-member-variables-in-classes)
 
     /**
+    @brief Requires the @ref machine to call a user-provided `on_unprocessed()`
+    member function whenever a call to @ref machine::process_event() doesn't lead to
+    any state transition.
+
+    The said member function must have the following form:
+    @code
+    void on_unprocessed(const event_type& event);
+    @endcode
+
+    State machine definitions typically define several overloads of this
+    function, for all events of interest.
+
+    Example:
+    @code
+    struct machine_def
+    {
+        static constexpr auto conf = machine_conf_c
+            .enable_on_unprocessed()
+            //...
+        ;
+
+        void on_unprocessed(const some_event_type& event)
+        {
+            //...
+        }
+
+        void on_unprocessed(const some_other_event_type& event)
+        {
+            //...
+        }
+
+        //Ignore all other event types
+        template<class Event>
+        void on_unprocessed(const Event&)
+        {
+            //nothing
+        }
+
+        //...
+    };
+    @endcode
+    */
+    bool on_unprocessed = false; //NOLINT(misc-non-private-member-variables-in-classes)
+
+    /**
+    @brief Requires the @ref machine to call a user-provided `pretty_name()` static
+    member function to get the pretty name of the state machine.
+
+    See `maki::pretty_name`.
+
+    Example:
+    @code
+    struct machine_def
+    {
+        static constexpr auto conf = machine_conf_c
+            .enable_pretty_name()
+            //...
+        ;
+
+        static auto pretty_name()
+        {
+            return "Main FSM";
+        }
+
+        //...
+    };
+    @endcode
+    */
+    bool pretty_name_fn = false; //NOLINT(misc-non-private-member-variables-in-classes)
+
+    /**
+    @brief Disables run-to-completion.
+
+    This makes the state machine much faster, but you have to make sure you
+    <b>never</b> call @ref machine::process_event() recursively.
+
+    Use it at your own risk!
+    */
+    bool run_to_completion = true; //NOLINT(misc-non-private-member-variables-in-classes)
+
+    /**
+    @brief Maximum object alignment requirement for the run-to-completion event
+    queue to enable small object optimization (and thus avoid an extra memory
+    allocation).
+    */
+    std::size_t small_event_max_align = 8; //NOLINT(misc-non-private-member-variables-in-classes, cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+
+    /**
+    @brief Maximum object size for the run-to-completion event queue to enable
+    small object optimization (and thus avoid an extra memory allocation).
+    */
+    std::size_t small_event_max_size = 16; //NOLINT(misc-non-private-member-variables-in-classes, cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+
+    /**
     @brief The list of transition table types. One region per transmission table
     is created.
     */
@@ -420,16 +419,16 @@ struct machine_conf
     [[maybe_unused]] const auto arg_auto_start = auto_start; \
     [[maybe_unused]] const auto arg_before_state_transition = before_state_transition; \
     [[maybe_unused]] const auto arg_context_type = context_type; \
-    [[maybe_unused]] const auto arg_run_to_completion = run_to_completion; \
-    [[maybe_unused]] const auto arg_pretty_name_fn = pretty_name_fn; \
-    [[maybe_unused]] const auto arg_on_exception = on_exception; \
-    [[maybe_unused]] const auto arg_on_unprocessed = on_unprocessed; \
-    [[maybe_unused]] const auto arg_small_event_max_align = small_event_max_align; \
-    [[maybe_unused]] const auto arg_small_event_max_size = small_event_max_size; \
+    [[maybe_unused]] const auto arg_on_entry = on_entry; \
     [[maybe_unused]] const auto arg_on_event = on_event; \
     [[maybe_unused]] const auto arg_on_event_auto = on_event_auto; \
-    [[maybe_unused]] const auto arg_on_entry = on_entry; \
+    [[maybe_unused]] const auto arg_on_exception = on_exception; \
     [[maybe_unused]] const auto arg_on_exit = on_exit; \
+    [[maybe_unused]] const auto arg_on_unprocessed = on_unprocessed; \
+    [[maybe_unused]] const auto arg_pretty_name_fn = pretty_name_fn; \
+    [[maybe_unused]] const auto arg_run_to_completion = run_to_completion; \
+    [[maybe_unused]] const auto arg_small_event_max_align = small_event_max_align; \
+    [[maybe_unused]] const auto arg_small_event_max_size = small_event_max_size; \
     [[maybe_unused]] const auto arg_transition_tables = transition_tables; \
  \
     { \
@@ -446,16 +445,16 @@ struct machine_conf
             arg_auto_start, \
             arg_before_state_transition, \
             arg_context_type, \
-            arg_run_to_completion, \
-            arg_pretty_name_fn, \
-            arg_on_exception, \
-            arg_on_unprocessed, \
-            arg_small_event_max_align, \
-            arg_small_event_max_size, \
+            arg_on_entry, \
             arg_on_event, \
             arg_on_event_auto, \
-            arg_on_entry, \
+            arg_on_exception, \
             arg_on_exit, \
+            arg_on_unprocessed, \
+            arg_pretty_name_fn, \
+            arg_run_to_completion, \
+            arg_small_event_max_align, \
+            arg_small_event_max_size, \
             arg_transition_tables \
         }; \
     }

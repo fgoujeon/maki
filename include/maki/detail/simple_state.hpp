3608b7a94a4a908401dc368cc36928f94187e07f
Florian Goujeon <florian.goujeon@gmail.com>
2024-02-03 17:54:59 +0100

    Split simple_state into simple_state and simple_state_no_context

--- include/maki/detail/simple_state.hpp
+++ include/maki/detail/simple_state.hpp
@@ -18,6 +18,92 @@
 namespace maki::detail
 {
 
+template<const auto& Conf>
+class simple_state_no_context
+{
+public:
+    using option_set_type = std::decay_t<decltype(opts(Conf))>;
+
+    static constexpr auto context_sig = opts(Conf).context_sig;
+
+    template<class Machine, class Context, class Event>
+    void call_entry_action(Machine& mach, Context& ctx, const Event& event)
+    {
+        if constexpr(!tlu::empty_v<entry_action_cref_constant_list>)
+        {
+            /*
+            If at least one entry action is defined, state is required to define
+            entry actions for all possible event types.
+            */
+            call_matching_event_action<entry_action_cref_constant_list>
+            (
+                mach,
+                ctx,
+                event
+            );
+        }
+    }
+
+    template<class Machine, class Context, class Event, class... MaybeBool>
+    void call_internal_action(Machine& mach, Context& ctx, const Event& event, MaybeBool&... processed)
+    {
+        /*
+        Caller is supposed to check an interal action exists for the given event
+        type before calling this function.
+        */
+        static_assert(!tlu::empty_v<internal_action_cref_constant_list>);
+
+        call_matching_event_action<internal_action_cref_constant_list>
+        (
+            mach,
+            ctx,
+            event
+        );
+
+        maybe_bool_util::set_to_true(processed...);
+    }
+
+    template<class Machine, class Context, class Event>
+    void call_exit_action(Machine& mach, Context& ctx, const Event& event)
+    {
+        if constexpr(!tlu::empty_v<exit_action_cref_constant_list>)
+        {
+            /*
+            If at least one exit action is defined, state is required to define
+            entry actions for all possible event types.
+            */
+            call_matching_event_action<exit_action_cref_constant_list>
+            (
+                mach,
+                ctx,
+                event
+            );
+        }
+    }
+
+    template<class Event>
+    static constexpr bool has_internal_action_for_event()
+    {
+        return tlu::contains_if_v
+        <
+            internal_action_cref_constant_list,
+            event_action_traits::for_event<Event>::template has_matching_event_filter
+        >;
+    }
+
+    static constexpr const auto& conf = Conf;
+
+private:
+    static constexpr auto entry_actions = opts(Conf).entry_actions;
+    using entry_action_cref_constant_list = tuple_to_constant_list_t<entry_actions>;
+
+    static constexpr auto internal_actions = opts(Conf).internal_actions;
+    using internal_action_cref_constant_list = tuple_to_constant_list_t<internal_actions>;
+
+    static constexpr auto exit_actions = opts(Conf).exit_actions;
+    using exit_action_cref_constant_list = tuple_to_constant_list_t<exit_actions>;
+};
+
 /*
 Implementation of a non-composite state
 */
@@ -77,83 +163,36 @@ public:
     template<class Machine, class Context, class Event>
     void call_entry_action(Machine& mach, Context& parent_ctx, const Event& event)
     {
-        if constexpr(!tlu::empty_v<entry_action_cref_constant_list>)
-        {
-            /*
-            If at least one entry action is defined, state is required to define
-            entry actions for all possible event types.
-            */
-            call_matching_event_action<entry_action_cref_constant_list>
-            (
-                mach,
-                context_or(parent_ctx),
-                event
-            );
-        }
+        impl_.call_entry_action(mach, context_or(parent_ctx), event);
     }
 
     template<class Machine, class Context, class Event, class... MaybeBool>
     void call_internal_action(Machine& mach, Context& parent_ctx, const Event& event, MaybeBool&... processed)
     {
-        /*
-        Caller is supposed to check an interal action exists for the given event
-        type before calling this function.
-        */
-        static_assert(!tlu::empty_v<internal_action_cref_constant_list>);
-
-        call_matching_event_action<internal_action_cref_constant_list>
-        (
-            mach,
-            context_or(parent_ctx),
-            event
-        );
-
-        maybe_bool_util::set_to_true(processed...);
+        impl_.call_internal_action(mach, context_or(parent_ctx), event, processed...);
     }
 
     template<class Machine, class Context, class Event>
     void call_exit_action(Machine& mach, Context& parent_ctx, const Event& event)
     {
-        if constexpr(!tlu::empty_v<exit_action_cref_constant_list>)
-        {
-            /*
-            If at least one exit action is defined, state is required to define
-            entry actions for all possible event types.
-            */
-            call_matching_event_action<exit_action_cref_constant_list>
-            (
-                mach,
-                context_or(parent_ctx),
-                event
-            );
-        }
+        impl_.call_exit_action(mach, context_or(parent_ctx), event);
     }
 
     template<class Event>
     static constexpr bool has_internal_action_for_event()
     {
-        return tlu::contains_if_v
-        <
-            internal_action_cref_constant_list,
-            event_action_traits::for_event<Event>::template has_matching_event_filter
-        >;
+        return impl_type::template has_internal_action_for_event<Event>();
     }
 
     static constexpr const auto& conf = Conf;
 
 private:
+    using impl_type = simple_state_no_context<Conf>;
+
     static constexpr bool has_own_context = !std::is_same_v<context_type, null_t>;
 
-    static constexpr auto entry_actions = opts(Conf).entry_actions;
-    using entry_action_cref_constant_list = tuple_to_constant_list_t<entry_actions>;
-
-    static constexpr auto internal_actions = opts(Conf).internal_actions;
-    using internal_action_cref_constant_list = tuple_to_constant_list_t<internal_actions>;
-
-    static constexpr auto exit_actions = opts(Conf).exit_actions;
-    using exit_action_cref_constant_list = tuple_to_constant_list_t<exit_actions>;
-
     context_holder<context_type, context_sig> ctx_holder_;
+    impl_type impl_;
 };
 
 } //namespace

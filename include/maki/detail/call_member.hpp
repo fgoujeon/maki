8e23d3a6d4cd68a189e6df5ab4979a1e9e251e4c
Florian Goujeon <florian.goujeon@gmail.com>
2024-12-27 15:07:38 +0100

    Remove unused code

--- include/maki/detail/call_member.hpp
+++ include/maki/detail/call_member.hpp
@@ -7,47 +7,13 @@
 #ifndef MAKI_DETAIL_CALL_MEMBER_HPP
 #define MAKI_DETAIL_CALL_MEMBER_HPP
 
-#include "type_traits.hpp"
 #include "tlu.hpp"
 #include "../action.hpp"
 #include "../guard_signature.hpp"
 #include <functional>
-#include <type_traits>
 
 namespace maki::detail
 {
-
-template<class Fn, class Machine, class Context, class Event>
-auto call_action_or_guard
-(
-    const Fn& fun,
-    [[maybe_unused]] Machine& mach,
-    [[maybe_unused]] Context& ctx,
-    [[maybe_unused]] const Event& event
-)
-{
-    if constexpr(std::is_invocable_v<Fn, Context&, Machine&, const Event&>)
-    {
-        return std::invoke(fun, ctx, mach, event);
-    }
-    else if constexpr(std::is_invocable_v<Fn, Context&, const Event&>)
-    {
-        return std::invoke(fun, ctx, event);
-    }
-    else if constexpr(std::is_invocable_v<Fn, Context&>)
-    {
-        return std::invoke(fun, ctx);
-    }
-    else if constexpr(is_nullary_v<Fn>)
-    {
-        return std::invoke(fun);
-    }
-    else
-    {
-        constexpr auto is_false = sizeof(Machine) == 0;
-        static_assert(is_false, "No valid signature found for action/guard");
-    }
-}
 
 template<class Signature, Signature Sig, class Callable, class Context, class Machine, class Event>
 auto invoke_action_or_guard_callable

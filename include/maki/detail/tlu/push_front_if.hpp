41ad854839e2f8df3643b101fb5f43572e6bd8a1
Florian Goujeon <florian.goujeon@gmail.com>
2025-12-27 01:44:02 +0100

    Simplify and slightly improve build time of `tlu::filter`

---
+++ include/maki/detail/tlu/push_front_if.hpp
@@ -0,0 +1,36 @@
+//Copyright Florian Goujeon 2021 - 2025.
+//Distributed under the Boost Software License, Version 1.0.
+//(See accompanying file LICENSE or copy at
+//https://www.boost.org/LICENSE_1_0.txt)
+//Official repository: https://github.com/fgoujeon/maki
+
+#ifndef MAKI_DETAIL_TLU_PUSH_FRONT_IF_HPP
+#define MAKI_DETAIL_TLU_PUSH_FRONT_IF_HPP
+
+namespace maki::detail::tlu
+{
+
+/*
+`push_front_if` adds `U` to the front of `TList` if `Condition` is `true`.
+*/
+template<class TList, class U, bool Condition>
+struct push_front_if;
+
+template<class TList, class U>
+struct push_front_if<TList, U, false>
+{
+    using type = TList;
+};
+
+template<template<class...> class TList, class... Ts, class U>
+struct push_front_if<TList<Ts...>, U, true>
+{
+    using type = TList<U, Ts...>;
+};
+
+template<class TList, class U, bool Condition>
+using push_front_if_t = typename push_front_if<TList, U, Condition>::type;
+
+} //namespace
+
+#endif

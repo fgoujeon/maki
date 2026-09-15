97f0648f8e9480560c86be3013ba194bba21fa4f
Florian Goujeon <florian.goujeon@gmail.com>
2026-09-15 12:00:57 +0200

    Fix MSVC build

--- include/maki/detail/state_impls/simple_no_context.hpp
+++ include/maki/detail/state_impls/simple_no_context.hpp
@@ -121,6 +121,8 @@ public:
                 event
             );
         }
+
+        co_return;
     }
 #endif
 
@@ -207,6 +209,8 @@ public:
                 event
             );
         }
+
+        co_return;
     }
 #endif
 
